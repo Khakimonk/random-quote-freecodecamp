9206363a3fd331421eca2d8f40f33c4272c566b5
James Bint <khakimonk@gmail.com>
2016-04-26 00:30:36 +0100

    quote objects added

--- quote.js
+++ quote.js
@@ -3,3 +3,29 @@ var getHour = today.toDateString();
 
 var writeHour = document.getElementById('timestamp');
 writeHour = document.write('<h3>' + getHour + '</h3>');
+
+function myQuote(key, quoteText, Author) {
+    this.key = key;
+    this.quoteText = quoteText;
+    this.Author = Author;
+}
+
+var firstQuote = new myQuote(1, 'Life is a gift horse in my opinion.', ' J. D. Salinger');
+var secondQuote = new myQuote(2, 'It pays to be obvious, especially if you have a reputation for subtlety. ', 'Isaac Asimov');
+var thirdQuote = new myQuote(3, 'The world is changing: I feel it in the water, I feel it in the earth, and I smell it in the air.', 'J. R. R. Tolkien in The Return of the King')
+
+var newQuote, quoteEl, quoteText;
+
+quoteEl = document.getElementById('quoteBody');
+newQuote = document.createElement('p');
+quoteText = document.createTextNode(secondQuote.quoteText);
+newQuote.appendChild(quoteText);
+quoteEl.appendChild(newQuote);
+
+var quoteAuth, AuthEl, newAuth;
+
+AuthEl = document.getElementById('authBox');
+newAuth = document.createElement('p');
+quoteAuth = document.createTextNode(secondQuote.Author);
+newAuth.appendChild(quoteAuth);
+AuthEl.appendChild(newAuth);
